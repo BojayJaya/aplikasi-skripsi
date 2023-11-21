4c292301f9db59910ca54ffd2015627920cb6872
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-11-21 18:40:30 +0700

    Update skripsi.py

--- skripsi.py
+++ skripsi.py
@@ -2,25 +2,13 @@ from streamlit_option_menu import option_menu
 import streamlit as st
 import pandas as pd
 import regex as re
-import json
 import nltk
-import string
-nltk.download('stopwords')
-nltk.download('punkt')
-# from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-from nltk.tokenize import RegexpTokenizer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-# import pickle5 as pickle 
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.svm import SVC
 import warnings
-import seaborn as sns
-import matplotlib.pyplot as plt
-# from pickle import dump
 
 warnings.filterwarnings('ignore')
 
@@ -38,8 +26,8 @@ st.set_page_config(
 st.write("""
 <center><h2 style = "text-align: justify;">ANALISIS SENTIMEN JAMU MADURA MENGGUNAKAN ALGORITMA SUPPORT VECTOR MACHINE DAN QUERY EXPANSION RANKING</h2></center>
 """,unsafe_allow_html=True)
-#st.write("### Dosen Pembimbing I: .",unsafe_allow_html=True)
-#st.write("### Dosen Pembimbing II: .",unsafe_allow_html=True)
+#st.write("### Dosen Pembimbing I: Dr. Rika Yunitarini, ST., MT.",unsafe_allow_html=True)
+#st.write("### Dosen Pembimbing II: Fifin Ayu Mufarroha, S.Kom., M.Kom.",unsafe_allow_html=True)
 
 with st.container():
     with st.sidebar:
@@ -79,222 +67,80 @@ with st.container():
 
         st.title("Preprocessing Dataset Teks")
 
-        # Load the dataset
-        @st.cache
-        def load_dataset():
-            df = pd.read_csv('dataset.csv')
-            return df
-
-        # Preprocessing function
-        def preprocess_text(text):
-            # Case Folding
-            text = text.lower()
-
-            # Punctuation Removal
-            text = re.sub(r'[^\w\s]', '', text)
-
-            # Hapus tweet khusus
-            text = text.replace('\\t', " ").replace('\\n', " ").replace('\\u', " ").replace('\\', "")
-            text = text.encode('ascii', 'replace').decode('ascii')
-            text = ' '.join(re.sub("([@#][A-Za-z0-9]+)|(\w+:\/\/\S+)", " ", text).split())
-            text = text.replace("http://", " ").replace("https://", " ")
-
-            # Hapus nomor
-            text = re.sub(r"\d+", "", text)
-
-            # Hapus tanda baca
-            translator = str.maketrans('', '', string.punctuation)
-            text = text.translate(translator)
-
-            # Hapus whitespace
-            text = text.strip()
-            text = re.sub('\s+', ' ', text)
-
-            # Hapus karakter tunggal
-            text = re.sub(r"\b[a-zA-Z]\b", "", text)
-
-            # Tokenisasi
-            tokenizer = RegexpTokenizer(r'\w+')
-            tokens = tokenizer.tokenize(text)
-
-            # Stopword Removal
-            stop_words = set(stopwords.words('indonesian'))
-            filtered_tokens = [word for word in tokens if word not in stop_words]
-
-            # Stemming
-            factory = StemmerFactory()
-            stemmer = factory.create_stemmer()
-            stemmed_tokens = [stemmer.stem(token) for token in filtered_tokens]
-
-            # Gabungkan token kembali ke dalam teks
-            preprocessed_text = ' '.join(stemmed_tokens)
-
-            return preprocessed_text
-
-        st.title("Preprocessing Dataset Teks")
         st.write("### Dataset Sebelum Preprocessing")
-        # Load the dataset
-        df = load_dataset()
-        # Display the dataset before preprocessing
-        st.write(df)
+        dt_sblm_p = pd.read_csv("dt_sblm_p.csv")
+        st.write(dt_sblm_p)
 
-        # Apply the preprocessing function to the 'ulasan' column
-        df['preprocessed_text'] = df['ulasan'].apply(preprocess_text)
-
-        # Display the dataset with all preprocessing steps
-        st.write("### Dataset Setelah Preprocessing")
-        st.write(df[['ulasan', 'preprocessed_text']])
-
+        st.write("#### Dataset Setelah Preprocessing")
+        dt_stlh_p = pd.read_csv("dt_stlh_p.csv")
+        st.write(dt_stlh_p)
+        
     elif selected == "Implementation":
-        #Getting input from user
-        word = st.text_area('Masukkan kata yang akan di analisa :')
-
-        submit = st.button("submit")
+        text = st.text_area('Masukkan kata yang akan di analisa:')
+        submit = st.button("Submit")
 
         if submit:
-            def prep_input_data(word, slang_dict):
-                #Lowercase data
-                lower_case_isi = word.lower()
+            def preprocessing_data(text):
+                text = text.lower()
+                text = re.sub("\n", " ", text)
+                text = re.sub('https?://\S+|www\.\S+', '', text)
+                text = re.sub(r'[^\x00-\x7F]+', '', text)
+                text = re.sub(r'\d+', '', text)
 
-                #Cleansing dataset
-                clean_symbols = re.sub("[^a-zA-Zï ]+"," ", lower_case_isi)
+                tokens = nltk.word_tokenize(text)
 
-                #Slang word removing
-                def replace_slang_words(text):
-                    words = nltk.word_tokenize(text.lower())
-                    words_filtered = [word for word in words if word not in stopwords.words('indonesian')]
-                    for i in range(len(words_filtered)):
-                        if words_filtered[i] in slang_dict:
-                            words_filtered[i] = slang_dict[words_filtered[i]]
-                    return ' '.join(words_filtered)
-                slang = replace_slang_words(clean_symbols)
+                list_stopwords = stopwords.words('indonesian')
+                list_stopwords.extend(["yg", "dg", "rt", "dgn", "ny", "d", 'klo',
+                                    'kalo', 'amp', 'biar', 'bikin', 'bilang',
+                                    'gak', 'ga', 'krn', 'nya', 'nih', 'sih',
+                                    'si', 'tau', 'tdk', 'tuh', 'utk', 'ya',
+                                    'jd', 'jgn', 'sdh', 'aja', 'n', 't', 'e',
+                                    'nyg', 'hehe', 'pen', 'u', 'nan', 'loh', 'rt',
+                                    '&amp', 'yah'])
+                txt_stopword = pd.read_csv("https://raw.githubusercontent.com/masdevid/ID-Stopwords/master/id.stopwords.02.01.2016.txt",
+                                        names=["stopwords"], header=None)
+                list_stopwords.extend(txt_stopword["stopwords"][0].split(' '))
+                list_stopwords = set(list_stopwords)
+                tokens = [word for word in tokens if word not in list_stopwords]
 
-                #Inisialisai fungsi tokenisasi dan stopword
-                # stop_factory = StopWordRemoverFactory()
-                tokenizer = RegexpTokenizer(r'dataran\s+tinggi|jawa\s+tengah|[\w\']+')
-                tokens = tokenizer.tokenize(slang)
-
-                stop_factory = StopWordRemoverFactory()
-                more_stopword = ["yg", "dg", "rt", "dgn", "ny", "d", 'klo', 'kalo', 'amp', 'biar', 'bikin', 'bilang',
-                                'gak', 'ga', 'krn', 'nya', 'nih', 'sih', 'si', 'tau', 'tdk', 'tuh', 'utk', 'ya',
-                                'jd', 'jgn', 'sdh', 'aja', 'n', 't', 'nyg', 'hehe', 'pen', 'u', 'nan', 'loh', 'rt',
-                                '&amp', 'yah']
-                data = stop_factory.get_stop_words()+more_stopword
-                removed = []
-                if tokens not in data:
-                    removed.append(tokens)
-
-                #list to string
-                gabung =' '.join([str(elem) for elem in removed])
-
-                #Steaming Data
                 factory = StemmerFactory()
                 stemmer = factory.create_stemmer()
-                stem = stemmer.stem(gabung)
-                return lower_case_isi,clean_symbols,slang,gabung,stem
-            
-            #Kamus
-            with open('combined_slang_words.txt') as f:
-                data = f.read()
-            slang_dict = json.loads(data)
+                tokens = [stemmer.stem(term) for term in tokens]
 
-            #Dataset
-            Data_ulasan = pd.read_csv("dataprep.csv")
-            ulasan_dataset = Data_ulasan['ulasan']
-            sentimen = Data_ulasan['label']
+                return ' '.join(tokens)
 
-            # TfidfVectorizer 
+            Dt_Ujm = pd.read_csv("dt_stlh_p.csv")
+            ulasan_dataset = Dt_Ujm['ulasan']
+            sentimen = Dt_Ujm['label']
+
+            # Text preprocessing
+            ulasan_dataset_preprocessed = [preprocessing_data(ulasan) for ulasan in ulasan_dataset]
+
+            # TF-IDF Vectorization
             tfidfvectorizer = TfidfVectorizer(analyzer='word')
-            tfidf_wm = tfidfvectorizer.fit_transform(ulasan_dataset)
-            tfidf_tokens = tfidfvectorizer.get_feature_names_out()
-            df_tfidfvect = pd.DataFrame(data = tfidf_wm.toarray(),columns = tfidf_tokens)
-            
-            #Train test split
-            # Memisahkan data menjadi training set dan test set
+            tfidf_wm = tfidfvectorizer.fit_transform(ulasan_dataset_preprocessed)
+
+            # Split data into train and test sets
             X_train, X_test, y_train, y_test = train_test_split(tfidf_wm, sentimen, test_size=0.1, random_state=1)
 
-            # model
-            # with open('modelpola.pkl', 'rb') as file:
-            #     loaded_model = pickle.load(file)
-            # clf = loaded_model.fit(X_train,y_train)
-            # y_pred=clf.predict(X_test)
+            # Train Support Vector Machine (SVM) classifier
+            svm_clf = SVC(kernel='linear', C=1.0)
+            svm_clf.fit(X_train, y_train)
 
-            # Membuat instance objek dari kelas MultinomialNB
-            clf = MultinomialNB()
+            # Predict on the test set
+            y_pred = svm_clf.predict(X_test)
 
-            # Melatih model menggunakan data pelatihan
-            clf.fit(X_train, y_train)
+            # Streamlit implementation
+            st.subheader('Text Analysis with SVM')
+            preprocessed_text = preprocessing_data(text)
+            v_data = tfidfvectorizer.transform([preprocessed_text])
+            y_preds = svm_clf.predict(v_data)
 
-            # Membuat prediksi menggunakan data testing
-            y_pred = clf.predict(X_test)
-
-            #Evaluasi
-            # Menghitung akurasi
-            akurasi = accuracy_score(y_test, y_pred)
-
-            # Inputan 
-            lower_case_isi,clean_symbols,slang,gabung,stem = prep_input_data(word, slang_dict)
-            
-            # #Prediksi
-            v_data = tfidfvectorizer.transform([stem]).toarray()
-            y_preds = clf.predict(v_data)
-
-            st.subheader('Preprocessing')
-            st.write(pd.DataFrame([lower_case_isi],columns=["Case Folding"]))
-            st.write(pd.DataFrame([clean_symbols],columns=["Cleansing"]))
-            st.write(pd.DataFrame([slang],columns=["Slang Word Removing"]))
-            st.write(pd.DataFrame([gabung],columns=["Stop Word Removing"]))
-            st.write(pd.DataFrame([stem],columns=["Steaming"]))
-            # st.write('Case Folding')
-            # st.write(lower_case_isi)
-            # st.write('Cleaning Simbol')
-            # st.write(clean_symbols)
-            # st.write('Slang Word Removing')
-            # st.write(slang)
-            # st.write('Stop Word Removing')
-            # st.write(gabung)
-            # st.write('Steaming')
-            # st.write(stem)
-
-            # st.subheader('Akurasi')
-            # st.info(akurasi)
-            # Mengubah akurasi menjadi persen dengan 2 desimal
-            akurasi_persen = akurasi * 100
-
-            # Menampilkan akurasi
-            st.subheader('Akurasi')
-            st.info(f"{akurasi_persen:.2f}%")
-
-            
-
-            st.subheader('Prediksi')
-            if y_preds == "positive":
+            st.subheader('Prediction:')
+            if y_preds[0] == "positive":
                 st.success('Positive')
             else:
                 st.error('Negative')
-
-            # Classification Report
-            classification_rep = classification_report(y_test, y_pred)
-            st.subheader('Classification Report:\n')
-            st.code(classification_rep)
-
-            # # Confusion Matrix Heatmap
-            # st.subheader('Confusion Matrix Heatmap')
-            # fig, ax = plt.subplots()
-            # sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Negative", "Positive"], yticklabels=["Negative", "Positive"], ax=ax)
-            # st.pyplot(fig)
-
-            # Menghitung confusion matrix
-            cm = confusion_matrix(y_test, y_pred)
-
-            # Menampilkan confusion matrix dengan plot
-            st.subheader('Confusion Matrix')
-            plt.figure(figsize=(8, 6))
-            sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-            plt.title('Confusion Matrix')
-            plt.xlabel('Predicted')
-            plt.ylabel('Actual')
-            st.pyplot()
 
     elif selected == "Tentang Kami":
         st.write("#####  Skripsi") 
