d036d68c8ddb52d727107efec6e834da7fa99895
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-11-24 18:58:42 +0700

    Update skripsi.py

--- skripsi.py
+++ skripsi.py
@@ -4,6 +4,7 @@ import pandas as pd
 import regex as re
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from sklearn.model_selection import train_test_split
