c61a218d2f5ab8bfcf05718c372fd9feb6ebc91f
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-11-26 12:27:54 +0700

    Update skripsi.py

--- skripsi.py
+++ skripsi.py
@@ -122,8 +122,8 @@ with st.container():
         df_akurasi['Akurasi'] = df_akurasi['Akurasi'].apply(lambda x: x * 100)
 
         # Membuat plot menggunakan Matplotlib
-        plt.figure(figsize=(6, 4))
-        bars = plt.bar(df_akurasi['Pembagian Dataset'], df_akurasi['Akurasi'], color='skyblue', width=0.6)
+        plt.figure(figsize=(6, 2))
+        bars = plt.bar(df_akurasi['Pembagian Dataset'], df_akurasi['Akurasi'], color='skyblue', width=0.3)
 
         # Menambahkan label akurasi di atas setiap bar
         for bar in bars:
