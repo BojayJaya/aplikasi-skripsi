b3ad4c52334059e19f964b9f2c898c39da4324f8
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2024-01-07 20:01:36 +0700

    Update skripsi.py

--- skripsi.py
+++ skripsi.py
@@ -130,14 +130,14 @@ with st.container():
 
         # Menambah label total positif di atas batang positif
         for i, value in enumerate(df_bulan['Positif']):
-            ax.text(i, value + 0.1, str(value), ha='center', va='bottom', color='black')
+            ax.text(i + 0.2, value + 0.1, str(value), ha='center', va='bottom', color='black')
 
         # Bar chart untuk Negatif (merah)
         ax.bar(df_bulan['Bulan'], df_bulan['Negatif'], color='red', label='Negatif', align='edge', width=-0.4)
 
         # Menambah label total negatif di atas batang negatif
         for i, value in enumerate(df_bulan['Negatif']):
-            ax.text(i, value + 0.1, str(value), ha='center', va='bottom', color='black')
+            ax.text(i - 0.2, value + 0.1, str(value), ha='center', va='bottom', color='black')
 
         # Menambah legenda, judul, dan label sumbu
         ax.legend()
